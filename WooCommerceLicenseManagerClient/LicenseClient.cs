78e3d610c39fee2d368734b581a5a89d53c539d5
Amen Jlili <amen@bluebyte.biz>
2024-11-06 13:05:17 -0800

    Upgraded to V12

--- WooCommerceLicenseManagerClient/LicenseClient.cs
+++ WooCommerceLicenseManagerClient/LicenseClient.cs
@@ -26,15 +26,31 @@ namespace WooCommerceLicenseManagerClient
         {
             return await ExecuteLicenseRequestAsync($"/wp-json/lmfwc/v2/licenses/validate/{licenseKey}", Method.GET);
         }
+        public   LicenseResponse ValidateLicense(string licenseKey)
+        {
+            return ExecuteLicenseRequest($"/wp-json/lmfwc/v2/licenses/validate/{licenseKey}", Method.GET);
+        }
+
+
 
         public async Task<LicenseResponse> DeactivateLicenseAsync(string licenseKey)
         {
             return await ExecuteLicenseRequestAsync($"/wp-json/lmfwc/v2/licenses/deactivate/{licenseKey}", Method.GET);
         }
 
+        public LicenseResponse DeactivateLicense(string licenseKey)
+        {
+            return ExecuteLicenseRequest($"/wp-json/lmfwc/v2/licenses/deactivate/{licenseKey}", Method.GET);
+        }
+
         public async Task<LicenseResponse> ActivateLicenseAsync(string licenseKey)
         {
             return await ExecuteLicenseRequestAsync($"/wp-json/lmfwc/v2/licenses/activate/{licenseKey}", Method.GET);
+        }
+
+        public   LicenseResponse ActivateLicense(string licenseKey)
+        {
+            return   ExecuteLicenseRequest($"/wp-json/lmfwc/v2/licenses/activate/{licenseKey}", Method.GET);
         }
 
         private async Task<LicenseResponse> ExecuteLicenseRequestAsync(string endpoint, Method method)
@@ -80,6 +96,51 @@ namespace WooCommerceLicenseManagerClient
             }
         }
 
+        public LicenseResponse ExecuteLicenseRequest(string endpoint, Method method)
+        {
+            var client = new RestClient(_baseUrl);
+            if (string.IsNullOrWhiteSpace(MachineNode) == false)
+                client.UserAgent = MachineNode;
+            var request = new RestRequest(endpoint, method);
+            request.AddHeader("Authorization", _authorizationHeader);
+
+            if (string.IsNullOrWhiteSpace(MachineNode) == false)
+                request.AddHeader("User-Agent", MachineNode);
+
+            var response = client.Execute(request);
+
+            if (response.IsSuccessful)
+            {
+                // Clean the response content by removing non-JSON content (e.g., HTML warnings)
+                var jsonResponse = ExtractJson(response.Content);
+                var licenseResponse = default(LicenseResponse);
+                try
+                {
+
+                    licenseResponse = JsonConvert.DeserializeObject<LicenseResponse>(jsonResponse);
+                }
+                catch (Exception e)
+                {
+
+                }
+
+                if (licenseResponse != null && licenseResponse.success)
+                {
+                    return licenseResponse;
+                }
+                else
+                {
+                    throw new Exception($"License request failed: {string.Join(", ", licenseResponse?.data?.errors?.LmfwcRestDataError)}");
+                }
+            }
+            else
+            {
+                throw new Exception($"Failed to execute license request. Status Code: {response.StatusCode}, Response: {response.Content}");
+            }
+        }
+
+
+
         // Method to extract the JSON portion from the mixed content response
         private string ExtractJson(string content)
         {
@@ -114,7 +175,7 @@ namespace WooCommerceLicenseManagerClient
         public int? ProductId { get; set; }
         public int? UserId { get; set; }
         public string LicenseKey { get; set; }
-        public DateTime ExpiresAt { get; set; }
+        public DateTime? ExpiresAt { get; set; }
         public int? ValidFor { get; set; }
         public int? Source { get; set; }
         public int? Status { get; set; }
